de53e6d1f2c0e6d0c06605a9e47797886201a5b7
rickvanv <91460011+rickvanv@users.noreply.github.com>
2023-02-13 22:09:35 +0100

    Add files via upload

--- tools/checking_solutions/check-MIS.py
+++ tools/checking_solutions/check-MIS.py
@@ -1,49 +1,71 @@
-import networkx as nx
-from pyvis.network import Network
-import fast_matrix_market as fmm
+from io import StringIO
+from scipy.io import mmread
 
-matrix = fmm.mmread("ca-GrQc-test.mtx")
+input_file = "ca-GrQc.mtx"
+matrix = mmread(input_file)
 newmatrix = matrix.tocsr()
-
 size = matrix.shape[0]
-print(size)
 
-
-mis_raw_in = input(
-    "Please enter your computed MIS to check if it is correct: ")
+mis_raw_file = open("../../output.txt", "r")
+mis_raw_in = mis_raw_file.read()[:-2]
 mis_list = mis_raw_in.split(', ')
 
 mis_fin = []
 for m in mis_list:
     mis_fin.append(int(m)-1)
 
-nodes = {0, 1}
+nodes = {0,1}
 for n in range(0, size):
     nodes.add(n)
 
-print("This is your entered MIS:")
-print(mis_fin)
-
-
-# this checks if there are too many nodes in the entered MIS:
+print("Checking if MIS found of %s is an IS" %(input_file))
 count = 0
 for i in mis_fin:
-    print("node %d" % (i))
     if (i not in nodes):
         count = count + 1
         print("wrong MIS - node %d has already a neighbouring node, which is part of the MIS, thus this node can't be part of this MIS" % (i))
+        break
     else:
         for j in range(0, size):
             if (newmatrix[i, j] == 1.0):
                 if (j in nodes):
-                    print("remove edge (%d, %d)" % (i, j))
                     nodes.remove(j)
-        print("removed node %d" % (i))
-        nodes.remove(i)
+        if (i not in nodes):
+            count = count + 1
+            print("wrong MIS - node %d has already a neighbouring node, which is part of the MIS, thus this node can't be part of this MIS" % (i))
+            break
+        else:
+            nodes.remove(i)
 
 if (count == 0):
-    print("No faults found")
+    print("No faults found, hence MIS found is an IS")
 else:
     print("There has been %d nodes found which already have neighbouring nodes in the MIS. Thus the entered MIS is not correct." % (count))
+    exit("Not even an IS")
+
+print("Checking if MIS found is an MIS")
+notamis = False
+for i in range(0,size):
+    if i in mis_fin:
+        continue
+    else:
+        nonzeros = [k for k in range(0,size) if newmatrix[i, k] == 1.0]
+        inmis = False
+        for j in nonzeros:
+            if j in mis_fin:
+                inmis = True
+                break
+        if not inmis:
+            notamis = True
+            break
+
+if notamis:
+    print("Found IS is not a MIS")
+else:
+    print("Found IS is a MIS!!")
+
+
+
+
 
 # it should be checked as well, if the entered node set is an actual MIS or only a IS!
